598886e9b78357d5e38c71fa45a2ae9423c59e00
Vladimir <vladimir.volobuev2k02@mail.ru>
2025-04-05 14:21:00 +0100

    first commit

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ BOT_TOKEN = '8101388926:AAEjCS7kwSp8EitsYo8m11rT4SeQzUsSf4M'
 
 def start(update: Update, context: CallbackContext):
     user_name = update.message.from_user.first_name
-    update.message.reply_text(f'Привет, {user_name}! 😊 Отправь мне голосовое сообщение, и я изменю его тон. 🎶 '
+    update.message.reply_text(f'Привет, {user_name}! 😊 Отправь мне голосовое сообщение, и я изменю его тон. 🎶\n'
                               'Разработчик - Владимир Волобуев @volobuevv 👨‍💻')
 
 def voice(update: Update, context: CallbackContext):
@@ -16,7 +16,7 @@ def voice(update: Update, context: CallbackContext):
     file.download(file_path)
 
     sound = AudioSegment.from_ogg(file_path)
-    octaves = -0.5  # понижение тональности
+    octaves = -0.5
     new_sample_rate = int(sound.frame_rate * (2 ** octaves))
     sound = sound._spawn(sound.raw_data, overrides={'frame_rate': new_sample_rate})
 
@@ -31,21 +31,20 @@ def voice(update: Update, context: CallbackContext):
     sound.export(output_mp3_path, format="mp3")
     sound.export(output_wav_path, format="wav")
 
-    # Отправляем измененный голос
     with open(output_ogg_path, 'rb') as f:
         update.message.reply_voice(voice=InputFile(f), caption="Вот твой голос с пониженным тоном!")
 
-    # Отправляем файлы для скачивания
     with open(output_ogg_path, 'rb') as f:
-        update.message.reply_document(document=InputFile(f), caption="Скачай OGG файл")
+        update.message.reply_document(document=InputFile(f))
 
     with open(output_mp3_path, 'rb') as f:
-        update.message.reply_document(document=InputFile(f), caption="Скачай MP3 файл")
+        update.message.reply_document(document=InputFile(f))
 
     with open(output_wav_path, 'rb') as f:
-        update.message.reply_document(document=InputFile(f), caption="Скачай WAV файл")
+        update.message.reply_document(document=InputFile(f))
 
-    # Удаляем временные файлы
+    update.message.reply_text('Разработчик - Владимир Волобуев @volobuevv 👨‍💻')
+
     os.remove(file_path)
     os.remove(output_ogg_path)
     os.remove(output_mp3_path)
